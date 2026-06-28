17e52c2116fd77115547131a6445a590305fc93d
jenniferxu1101 <shiyaoxu18@gmail.com>
2026-06-28 00:06:07 -0400

    cran note

--- R/CompCasual-package.R
+++ R/CompCasual-package.R
@@ -1,10 +1,5 @@
-#' CompCausal: Inferring causal effects in comprehensive cohort studies
+#' CompCausal
 #'
-#' This package provide R implementations of the estimation methods for 
-#' inferring causal effects in comprehensive cohort studies, 
-#' in the presence of unmeasured confounding and missing outcomes. The causal 
-#' effects include comprehensive cohort causal effects, randomized trial causal effects, 
-#' and patient preference causal effect. 
 #' @import utils
 #' @importFrom stats predict coef as.formula binomial dbeta dnorm integrate model.matrix nlminb optim optimize pnorm qnorm quantile sd uniroot var
 #' @importFrom betareg betareg
@@ -20,5 +15,6 @@
 #' @importFrom purrr map_dbl
 #' @importFrom Rcpp Module
 #' @import rlang
-#' @keywords Comprehensive cohort, Unmeasured confounding, Missing at random, Causal effect
+#' @keywords internal
+#' @noRd
 "_PACKAGE"
